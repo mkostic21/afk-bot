f910729769a827d1deb81072e644d79f3a56576a
Mimi <mislav.kostiac@gmail.com>
2021-07-30 14:26:23 +0200

    added menu loop

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace afk_bot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConsoleKey input;
+            do
+            {
+                //TODO: UI default splashscreen message here:
+                switch (input = Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.D1:
+                        //1st method here:
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("Exiting.");
+                        break;
+                    default:
+                        //help message here?
+                        break;
+                }
+            } while (!input.Equals(ConsoleKey.Escape));
         }
     }
 }
